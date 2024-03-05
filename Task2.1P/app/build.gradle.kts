48ab30aaa6fe80b839a61fc5eb712e1859e2f8d6
userstarwind <jiangjunkai21@gmail.com>
2024-03-05 19:47:49 +1100

    Task2.1P

--- Task2.1P/app/build.gradle.kts
+++ Task2.1P/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.task21p"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.task21p"
--- Task2.1P/app/src/main/java/com/example/task21p/WeightConverter.java
+++ Task2.1P/app/src/main/java/com/example/task21p/WeightConverter.java
@@ -1,2 +1,41 @@
-package com.example.task21p;public class WeightConverter {
+package com.example.task21p;
+
+public class WeightConverter implements IUnitConverter {
+    @Override
+    public double convert(double value, String fromUnit, String toUnit) {
+        double valueInGrams;
+        switch (fromUnit) {
+            case "pound":
+                valueInGrams = value * 453.59237;
+                break;
+            case "ounce":
+                valueInGrams = value * 28.34952;
+                break;
+            case "ton":
+                valueInGrams = value * 907185.0;
+                break;
+            case "kg":
+                valueInGrams = value * 1000;
+                break;
+            case "g":
+                valueInGrams = value;
+                break;
+            default:
+                return value;
+        }
+
+        switch (toUnit) {
+            case "pound":
+                return valueInGrams / 453.59237;
+            case "ounce":
+                return valueInGrams / 28.34952;
+            case "ton":
+                return valueInGrams / 907185.0;
+            case "kg":
+                return valueInGrams / 1000;
+            default:
+                return valueInGrams;
+        }
+    }
 }
+
